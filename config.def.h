16299a5d4d6b59cef5fc4f5a23caf1f5211b2d64
João Cunha <skidrow.jc@gmail.com>
2020-06-15 20:34:56 +0100

    Updated config and colors

--- config.def.h
+++ config.def.h
@@ -11,7 +11,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	              /*     fg         bg       */
         [SchemeNorm] = { "#f3f4f5", "#223345" },
-	[SchemeSel]  = { "#f3f4f5", "#BD4455" },
+	[SchemeSel]  = { "#f3f4f5", "#5294e2" },
 	[SchemeOut]  = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
